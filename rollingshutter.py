3315c9729fec9c499b270e5fa8fde066179da1bb
Emre Ozcan <EmreOzcan@users.noreply.github.com>
2017-07-13 11:01:59 +0300

    Added command-line arguments
    Arguments: -p -s -f -w -h

--- rollingshutter.py
+++ rollingshutter.py
@@ -1,31 +1,84 @@
-def rs(folder,speed=1):
-    
+from sys import argv
+
+print(__file__)
+path = __file__.split("/")[:-1]
+path_real = ""
+for layer in path:
+    path_real += layer + "/"
+print(path_real)
+
+
+options = {"path": "images", "speed": 1, "frame_file": "png", "width": 1920, "height": 1080}
+
+
+for x, i in enumerate(argv):
+    if i == "-p":
+        try:
+            options["path"] = argv[x+1]
+        except IndexError:
+            print("Error: -p option requires string folder path.")
+            exit(1)
+    if i == "-s":
+        try:
+            options["speed"] = int(argv[x+1])
+        except (IndexError, ValueError):
+            print("Error: -s option requires integer speed.")
+            exit(2)
+    if i == "-f":
+        try:
+            options["frame_file"] = argv[x+1]
+        except IndexError:
+            print("Error: -f option requires string frame extension")
+            exit(3)
+    if i == "-w":
+        try:
+            options["width"] = int(argv[x+1])
+        except (IndexError, ValueError):
+            print("Error: -w option requires integer width.")
+            exit(4)
+    if i == "-h":
+        try:
+            options["height"] = int(argv[x+1])
+        except (IndexError, ValueError):
+            print("Error: -h option requires integer height.")
+            exit(5)
+
+computed = {"frame_dir": path_real + str(options["path"]) + "/",
+            "output_dir": path_real}
+
+print("Local path:      "+options["path"])
+print("Global path:     "+computed["frame_dir"])
+print("Speed:           "+str(options["speed"]))
+print("Frame extension: "+options["frame_file"])
+print("Dimensions:      "+str(options["width"])+"x"+str(options["height"]))
+
+
+def rs(opts, comps):
+
     from PIL import Image
     import glob
-    
-    # ENTER YOUR DIRECTORIES AND FILE TYPE HERE
-    frame_dir = "/Users/mattparker/Documents/rollingshuttervideos/" + str(folder) + "/"
-    frame_file = "png"
-    output_dir = "/Users/mattparker/Documents/rollingshuttervideos/"
-    
-    width = 1920
-    height = 1080
-    
+
+    frame_dir = comps["frame_dir"]
+    frame_file = opts["frame_file"]
+    output_dir = comps["output_dir"]
+
+    width = opts["width"]
+    height = opts["height"]
+
     # Making our blank output frame
-    output_image = Image.new('RGB', (width, height)) 
-    
-    
+    output_image = Image.new('RGB', (width, height))
+
     # let us go through the frames one at a time
-    
+
     current_row = 0
-    
+
     for filename in glob.glob(frame_dir + "*." + frame_file):
         frame = Image.open(filename)
-        new_line = frame.crop((0, current_row, width, current_row+speed))
-        output_image.paste(new_line, (0,current_row))
-        current_row += speed
-    
+        new_line = frame.crop((0, current_row, width, current_row + opts["speed"]))
+        output_image.paste(new_line, (0, current_row))
+        current_row += opts["speed"]
+
     # and export the final frame
     output_image.save(output_dir + 'output_image.png')
-    
-    return 'DONE'
+
+rs(options, computed)
